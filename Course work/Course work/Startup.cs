0886d4b9736b6bff93435caadbc5b1b936c4bae5
Dmitri <lipov758@gmail.com>
2019-03-04 22:04:47 +0300

    Add SignalR

--- Course work/Course work/Startup.cs
+++ Course work/Course work/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Coursework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,7 @@ namespace Course_work
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +27,9 @@ namespace Course_work
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+           app.UseSignalR(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapHub<ChatHub>("/chatter");
             });
         }
     }
