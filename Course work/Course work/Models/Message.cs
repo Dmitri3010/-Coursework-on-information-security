594a55fd6171ca47a05e83292fdf5c3e01d6a0cf
Dmitri <lipov758@gmail.com>
2019-04-07 00:48:36 +0300

    Add users and message model

---
+++ Course work/Course work/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coursework.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string MessageContent { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public User user { get; set; }
+
+        public int UserId { get; set; }
+
+
+    }
+}
---
+++ Course work/Course work/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coursework.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string PasswordSalt { get; set; }
+
+        public string Role { get; set; }
+    }
+}
